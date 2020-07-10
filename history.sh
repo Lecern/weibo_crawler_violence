b91196a53569081e7404da3edc764e2ec320aab7
Jerry <gaozhiwei1993@gmail.com>
2020-07-10 21:55:19 +0900

    init

--- history.sh
+++ history.sh
@@ -6,14 +6,10 @@ then
     exit 1
 fi
 
-cd /home/gao/project/WeiboCrawler/WeiboSearch/
+cd /home/gao/project/weibo_crawler_violence/WeiboSearch/
 
-/home/gao/.pyenv/shims/python /home/gao/project/WeiboCrawler/WeiboSearch/login.py >> /home/gao/project/WeiboCrawler/WeiboSearch/log/cookie.log
+/home/gao/.pyenv/shims/python /home/gao/project/weibo_crawler_violence/WeiboSearch/login.py >> /home/gao/project/weibo_crawler_violence/WeiboSearch/log/cookie.log
 sleep 3
-/home/gao/.pyenv/shims/python /home/gao/project/WeiboCrawler/WeiboSearch/run.py --start=${1} --end=${2} >> /home/gao/project/WeiboCrawler/WeiboSearch/log/bash.log
+/home/gao/.pyenv/shims/python /home/gao/project/weibo_crawler_violence/WeiboSearch/run.py --start=${1} --end=${2} >> /home/gao/project/weibo_crawler_violence/WeiboSearch/log/bash.log
 sleep 3
-/home/gao/.pyenv/shims/python /home/gao/project/WeiboCrawler/WeiboSearch/run.py --keyword="冠状病毒" --start=${1} --end=${2} >> /home/gao/project/WeiboCrawler/WeiboSearch/log/bash.log
-sleep 3
-/home/gao/.pyenv/shims/python /home/gao/project/WeiboCrawler/WeiboSearch/run.py --keyword="新冠肺炎" --start=${1} --end=${2}>> /home/gao/project/WeiboCrawler/WeiboSearch/log/bash.log
-sleep 3
-/home/gao/.pyenv/shims/python /home/gao/project/WeiboCrawler/WeiboSearch/daily_word_cloud.py --date=${3} >> /home/gao/project/WeiboCrawler/WeiboSearch/log/upload_image.log
+/home/gao/.pyenv/shims/python /home/gao/project/weibo_crawler_violence/WeiboSearch/run.py --keyword="#面对家暴不再沉默#" --start=${1} --end=${2} >> /home/gao/project/weibo_crawler_violence/WeiboSearch/log/bash.log
