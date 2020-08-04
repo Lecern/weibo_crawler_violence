567a5d9e820d37f2d15ab2ce1a691d5d97e1fee7
Jerry <gaozhiwei1993@gmail.com>
2020-08-04 15:26:46 +0900

    bug fixed

--- crawler.sh
+++ crawler.sh
@@ -5,6 +5,6 @@ cd /home/gao/project/weibo_crawler_violence/WeiboSearch/
 
 /home/gao/.pyenv/shims/python /home/gao/project/weibo_crawler_violence/WeiboSearch/login.py >> /home/gao/project/weibo_crawler_violence/WeiboSearch/log/cookie.log
 sleep 3
-/home/gao/.pyenv/shims/python /home/gao/project/weibo_crawler_violence/WeiboSearch/run.py --start='2019-01-01' --end='2019-06-30' --ori >> /home/gao/project/weibo_crawler_violence/WeiboSearch/log/bash.log
+/home/gao/.pyenv/shims/python /home/gao/project/weibo_crawler_violence/WeiboSearch/run.py --start='2019-06-01' --end='2019-06-30' --ori >> /home/gao/project/weibo_crawler_violence/WeiboSearch/log/bash.log
 sleep 3
 /home/gao/.pyenv/shims/python /home/gao/project/weibo_crawler_violence/WeiboSearch/run.py --start='2020-01-01' --end='2020-06-30' --ori >> /home/gao/project/weibo_crawler_violence/WeiboSearch/log/bash.log
